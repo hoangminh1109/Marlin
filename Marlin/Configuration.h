c002f15ca0172ccfc77aa9c71642096b2149f2bf
hoangminh1109 <hoangminh1109@gmail.com>
2025-10-23 09:19:20 +0700

    update configuration

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2253,8 +2253,8 @@
 #define PREHEAT_1_FAN_SPEED     0 // Value from 0 to 255
 
 #define PREHEAT_2_LABEL       "PETG"
-#define PREHEAT_2_TEMP_HOTEND 240
-#define PREHEAT_2_TEMP_BED    85
+#define PREHEAT_2_TEMP_HOTEND 230
+#define PREHEAT_2_TEMP_BED    80
 #define PREHEAT_2_TEMP_CHAMBER 35
 #define PREHEAT_2_FAN_SPEED     0 // Value from 0 to 255
 
--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1454,7 +1454,7 @@
   #define LCD_DECIMAL_SMALL_XY
 
   // Show the E position (filament used) during printing
-  //#define LCD_SHOW_E_TOTAL
+  #define LCD_SHOW_E_TOTAL
 
   /**
    * LED Control Menu
